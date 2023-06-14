933d059027baec8be63072107e443da30ec5620a
sohanemon <m.sohanemon@gmail.com>
2023-06-14 18:09:47 +0000

    Jun 15, 2023, 12:09 AM

--- src/app/pricing.tsx
+++ src/app/pricing.tsx
@@ -1,13 +1,33 @@
 export default function Pricing() {
-  return <section></section>;
+  return (
+    <section>
+      {data.map((el, idx) => (
+        <PricingCard {...el} key={el.price} idx={idx} />
+      ))}
+    </section>
+  );
 }
 
-const PricingCard = () => {
-  return <></>;
+const PricingCard = ({
+  title,
+  price,
+  offeredPrice,
+}: (typeof data)[2] & { idx: number }) => {
+  return <div></div>;
 };
 
 const data = [
   {
-    title: '',
+    title: 'Best choice for individuals',
+    price: 60,
+  },
+  {
+    title: 'Best choice for individuals',
+    price: 99,
+  },
+  {
+    title: 'Best choice for priority support',
+    price: 160,
+    offeredPrice: 129,
   },
 ];
