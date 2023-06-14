c8113a73b959a1cc1de829de066f0e9a7e500fb8
sohanemon <m.sohanemon@gmail.com>
2023-06-14 18:44:58 +0000

    Jun 15, 2023, 12:44 AM

--- src/app/pricing.tsx
+++ src/app/pricing.tsx
@@ -24,7 +24,12 @@ const PricingCard = ({
   idx,
 }: (typeof data)[2] & { idx: number }) => {
   return (
-    <div className={cn('p-10 space-y-5', { '': offeredPrice })}>
+    <div
+      className={cn('p-10 space-y-5', {
+        'bg-gradient-to-r from-themeIndigo-dark to-themeIndigo-light rounded-lg':
+          offeredPrice,
+      })}
+    >
       <div className='flex gap-1'>
         <span className='text-xl font-semibold text-gray-500'>$</span>
         <p className='text-6xl font-semibold text-themeIndigo-light'>{price}</p>
