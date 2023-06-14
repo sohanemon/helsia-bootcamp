8c4cb07673b1eedc2da09cb3fdae5016b77ae12e
sohanemon <m.sohanemon@gmail.com>
2023-06-14 09:53:16 +0000

    Jun 14, 2023, 3:53 PM

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,5 +5,14 @@ export default function Button({
   className,
   ...props
 }: HtmlHTMLAttributes<HTMLButtonElement>) {
-  return <button className={cn('', {}, className)} {...props} />;
+  return (
+    <button
+      className={cn(
+        'h-12 bg-themeBlue rounded-xl text-white px-8',
+        {},
+        className
+      )}
+      {...props}
+    />
+  );
 }
