beffd310f7cc3f70d058dce2f776711ce3d5c672
sohanemon <m.sohanemon@gmail.com>
2023-06-14 18:00:45 +0000

    Jun 15, 2023, 12:00 AM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,12 @@ export default function Home() {
         rtl
         src='/assets/girl.png'
         heading='Passionate Team Dedicated to Your Health and Fitness'
-        grayText='Meet our highly skilled trainers who have years of experience and expertise in military-inspired training. They are passionate about helping people achieve their health and fitness goals.'
+        grayText='Our training program focuses on high-intensity military-inspired exercises that improve both physical and mental well-being. Each participant receives personalized training schedules and diet plans tailored to their goals.'
       />
       <TextImage
         src='/assets/team.svg'
-        heading='Passionate Team Dedicated to Your Health and Fitness'
-        grayText='Meet our highly skilled trainers who have years of experience and expertise in military-inspired training. They are passionate about helping people achieve their health and fitness goals.'
+        heading='Providing Food and Accommodation for your Journey'
+        grayText='During the training program, we provide healthy, delicious meals cooked from scratch. Enjoy a balanced diet that fuels your body for optimal performance.'
       />
     </main>
   );
