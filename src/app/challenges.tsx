c53d323e284e9a71350c019fd40988b15a075847
sohanemon <m.sohanemon@gmail.com>
2023-06-14 17:31:25 +0000

    Jun 14, 2023, 11:31 PM

--- src/app/challenges.tsx
+++ src/app/challenges.tsx
@@ -1,3 +1,33 @@
+import Image from 'next/image';
+
 export default function Challenges() {
-  return <></>;
+  return (
+    <section>
+      {data.map((el) => (
+        <div key={el.text}>
+          <div>
+            <Image src={el.icon} alt='icon' width={50} height={50} />
+          </div>
+          <div>
+            <p className='font-semibold leading-7'>{el.text}</p>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
+
+const data = [
+  {
+    text: 'Get that 11 line in 30 days',
+    icon: '/public/assets/crucified.svg',
+  },
+  {
+    text: '14 Days sherd challenge',
+    icon: '/public/assets/dancer.svg',
+  },
+  {
+    text: 'Get flat belly in 30 days',
+    icon: '/public/assets/dancer-motion.svg',
+  },
+];
