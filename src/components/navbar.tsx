750ef392f3fc1abd9eecae3dd32015dce823b08b
sohanemon <m.sohanemon@gmail.com>
2023-06-14 08:47:48 +0000

    Jun 14, 2023, 2:47 PM

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Navbar() {
   return (
     <nav>
-      <Image src={'/assets/logo.svg'} alt='logo' width={150} height={64} />
+      <Image src={'/assets/logo.svg'} alt='logo' width={148} height={64} />
     </nav>
   );
 }
