5363d02c945c8633c8b0ab701781e7b72cc5c2f6
sohanemon <m.sohanemon@gmail.com>
2023-06-14 08:42:55 +0000

    Jun 14, 2023, 2:42 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+      eligendi repellendus. Suscipit reprehenderit ex dolorem obcaecati odio
+      odit, nostrum, voluptatum a sit nisi quas assumenda excepturi voluptatem
+      aliquam. Nisi, ipsum.
+    </main>
+  );
 }
