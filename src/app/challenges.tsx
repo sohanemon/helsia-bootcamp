538f0cb6267d1f8ddda787dab8affa822499f186
sohanemon <m.sohanemon@gmail.com>
2023-06-14 17:41:37 +0000

    Jun 14, 2023, 11:41 PM

--- src/app/challenges.tsx
+++ src/app/challenges.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function Challenges() {
   return (
-    <section className='flex items-center justify-between gap-20 p-20 bg-gradient-to-r from-themeIndigo-dark to-themeIndigo-light rounded-3xl'>
+    <section className='flex items-center justify-between gap-20 p-20 my-20 bg-gradient-to-r from-themeIndigo-dark to-themeIndigo-light rounded-3xl'>
       {data.map((el) => (
         <div key={el.text} className='flex items-center gap-8'>
           <div className='p-4 rounded-lg bg-white/20'>
