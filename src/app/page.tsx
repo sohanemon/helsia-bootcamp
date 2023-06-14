48b1b5e8d0f961e0cf91cf52484d33b30918bf58
sohanemon <m.sohanemon@gmail.com>
2023-06-14 17:23:46 +0000

    Jun 14, 2023, 11:23 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Cta from '@/components/cta';
 import IntenseEffective from './intense-&-effective';
+import Challenges from './challenges';
 
 export default function Home() {
   return (
     <main>
       <Cta />
       <IntenseEffective />
+      <Challenges />
     </main>
   );
 }
