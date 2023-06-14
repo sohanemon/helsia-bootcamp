2f98026141574ddb6cf3066956d3c8580202b5f6
sohanemon <m.sohanemon@gmail.com>
2023-06-14 09:49:32 +0000

    Jun 14, 2023, 3:49 PM

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,7 @@
-export default function Button() {
+import { HtmlHTMLAttributes } from 'react';
+
+export default function Button({
+  className,
+}: HtmlHTMLAttributes<HTMLButtonElement>) {
   return <button></button>;
 }
