17d419763683ea16e9c1511598c403c6d2a27876
sohanemon <m.sohanemon@gmail.com>
2023-06-14 09:54:45 +0000

    Jun 14, 2023, 3:54 PM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import Navbar from '@/components/navbar';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 
-const poppins = Poppins({ subsets: ['latin'] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
 
 export const metadata = {
   title: 'Helsia Bootcamp',
