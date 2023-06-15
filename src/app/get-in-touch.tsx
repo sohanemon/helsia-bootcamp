6a621ebd307432951f109b112d369524037f7af7
sohanemon <m.sohanemon@gmail.com>
2023-06-15 08:57:52 +0000

    Jun 15, 2023, 2:57 PM

--- src/app/get-in-touch.tsx
+++ src/app/get-in-touch.tsx
@@ -1,6 +1,6 @@
 export default function GetInTouch() {
   return (
-    <section>
+    <section className='bg-gradient-to-r from-themeIndigo-dark to-themeIndigo-light'>
       <h1 className='text-4xl leading-normal text-center text-white '>
         Get In Touch
       </h1>
