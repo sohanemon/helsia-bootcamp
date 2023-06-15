17953d7cfb80308228c0bfee39621a1723515b48
sohanemon <m.sohanemon@gmail.com>
2023-06-15 06:48:17 +0000

    Jun 15, 2023, 12:48 PM

--- src/app/testimonial.tsx
+++ src/app/testimonial.tsx
@@ -7,3 +7,20 @@ export default function Testimonial() {
     </section>
   );
 }
+
+const data = [
+  {
+    image: '/assets/suketi.svg',
+    name: 'Suketi Mantapo',
+    occupation: 'Designer Graphic',
+    feedback:
+      "It's great to be able to work out from home and be helped by the helsia. My day feels fresher when I regularly participate in this fun sport. Good luck to the coaches.",
+  },
+  {
+    image: '/assets/ada.svg',
+    name: 'Ada Apose',
+    occupation: 'Designer Graphic',
+    feedback:
+      'exercise used to be a boring thing for me, but after following helsia I became fond of sports and sports became my new hobby. I participate in sports 5 times a week.',
+  },
+];
