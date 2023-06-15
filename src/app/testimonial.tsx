f6820d4248bc3610b1fc5124f4fb8a2ca3401fa5
sohanemon <m.sohanemon@gmail.com>
2023-06-15 06:54:33 +0000

    Jun 15, 2023, 12:54 PM

--- src/app/testimonial.tsx
+++ src/app/testimonial.tsx
@@ -1,12 +1,41 @@
+import GrayText from '@/components/ui/gray-text';
 import Heading from '@/components/ui/heading';
+import Image from 'next/image';
 
 export default function Testimonial() {
   return (
     <section>
       <Heading className='text-center'>Testimonials</Heading>
+      <div className='grid grid-cols-2 gap-12'>
+        {data.map((el) => (
+          <TestimonialCard key={el.feedback} {...el} />
+        ))}
+      </div>{' '}
     </section>
   );
 }
+
+const TestimonialCard = ({
+  feedback,
+  image,
+  name,
+  occupation,
+}: (typeof data)[0]) => {
+  return (
+    <div className='rounded-lg shadow-md p-11'>
+      <GrayText className='opacity-80'>{feedback}</GrayText>
+      <div>
+        <Image
+          src={image}
+          alt={name}
+          width={64}
+          height={69}
+          className='rounded-[1.2rem] bg-themeBg-ellipse-pink/70'
+        />
+      </div>
+    </div>
+  );
+};
 
 const data = [
   {
