d0922d857019e057662ff7494d547b4b931d4c7c
sohanemon <m.sohanemon@gmail.com>
2023-06-15 09:29:12 +0000

    Jun 15, 2023, 3:29 PM

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,10 +1,12 @@
 export default function Footer() {
   return (
-    <footer className='relative p-20 -mx-20 -mt-10 bg-themeBlue -z-20'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum deleniti
-      voluptatibus ea odio? Rem iure eos, dolore laborum provident officia animi
-      quia deserunt repellat consectetur veritatis ipsam obcaecati optio
-      repellendus.
+    <footer className='relative p-20 -mx-20 text-white -mt-14 bg-themeBlue -z-20 [&_p]:tracking-wider'>
+      <div>
+        <p>Logo</p>
+        <p className='text-sm opacity-60'>
+          We take care of your health with regular and fun exercise
+        </p>
+      </div>
     </footer>
   );
 }
