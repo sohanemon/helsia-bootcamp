15501983bb699017ba087b70569ddea9e3290c74
sohanemon <m.sohanemon@gmail.com>
2023-06-15 08:51:57 +0000

    Jun 15, 2023, 2:51 PM

--- src/app/get-in-touch.tsx
+++ src/app/get-in-touch.tsx
@@ -1,3 +1,9 @@
 export default function GetInTouch() {
-  return <></>;
+  return (
+    <section>
+      <h1 className='text-4xl leading-normal text-center text-white '>
+        Get In Touch
+      </h1>
+    </section>
+  );
 }
