89b1b1bfaf52c8ef41084182f539fce16898514d
sohanemon <m.sohanemon@gmail.com>
2023-06-15 09:33:48 +0000

    Jun 15, 2023, 3:33 PM

---
+++ src/app/social.tsx
@@ -0,0 +1,10 @@
+export default function Social() {
+  return <></>;
+}
+const icons = [
+  '/public/assets/facebook.svg',
+  '/public/assets/twitter.svg',
+  '/public/assets/instagram.svg',
+  '/public/assets/youtube.svg',
+  '/public/assets/discord.svg',
+];
