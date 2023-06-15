7d87817c587792ca1fd44f5fc9c7e53180845d21
sohanemon <m.sohanemon@gmail.com>
2023-06-15 09:01:11 +0000

    Jun 15, 2023, 3:01 PM

--- src/app/get-in-touch.tsx
+++ src/app/get-in-touch.tsx
@@ -4,6 +4,29 @@ export default function GetInTouch() {
       <h1 className='text-4xl leading-normal text-center text-white '>
         Get In Touch
       </h1>
+      <div>
+        {data.map((el) => (
+          <div key={el.title}></div>
+        ))}
+      </div>
     </section>
   );
 }
+
+const data = [
+  {
+    icon: '/assets/location.svg',
+    title: 'Location',
+    desc: 'Street 17, Block 3 New York',
+  },
+  {
+    icon: '/assets/phone.svg',
+    title: 'Contact Information',
+    desc: '+1 (323) 323-7423',
+  },
+  {
+    icon: '/assets/gmail.svg',
+    title: 'Email',
+    desc: 'helsia@gmail.com',
+  },
+];
